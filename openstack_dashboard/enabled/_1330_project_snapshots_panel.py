f7b8cc64df69c4a317db109fbae0a471da06b8d8
anak10thn <anak10thn@gmail.com>
2017-12-27 18:17:13 +0700

    FIX: snapshot

--- openstack_dashboard/enabled/_1330_project_snapshots_panel.py
+++ openstack_dashboard/enabled/_1330_project_snapshots_panel.py
@@ -3,9 +3,10 @@ PANEL = 'snapshots'
 # The slug of the dashboard the PANEL associated with. Required.
 PANEL_DASHBOARD = 'project'
 # The slug of the panel group the PANEL is associated with.
-PANEL_GROUP = 'volumes'
+# PANEL_GROUP = 'volumes'
+PANEL_GROUP = 'compute'
 
 # Python panel class of the PANEL to be added.
 ADD_PANEL = 'openstack_dashboard.dashboards.project.snapshots.panel.Snapshots'
 
-DISABLED = True
+DISABLED = False
